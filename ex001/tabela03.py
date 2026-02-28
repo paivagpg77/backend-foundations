a23a220f12f586668f2856cc4c405cbaddf3777c
paivagpg77 <paivag171@gmail.com>
2026-02-28 09:52:25 -0300

    ㅤ

--- ex001/tabela03.py
+++ ex001/tabela03.py
@@ -1,6 +1,7 @@
 import plotly.express as px
 import pandas as pd
 
+
 tabela = pd.read_csv('ClientesBanco.csv')
 grafico = px.histogram(tabela , x = 'Contatos 12m' , color  = 'Categoria')
 grafico.show()
